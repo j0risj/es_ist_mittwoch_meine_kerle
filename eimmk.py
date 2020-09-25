2a44aad7f91af6b95b95cc82c3509e1af91bbe95
j0risj <joma12912@gmail.com>
2020-09-25 04:18:23 +0200

    First working state

--- eimmk.py
+++ eimmk.py
@@ -1,20 +1,37 @@
-import praw
 import configparser
+import datetime
+import json
 import logging
 import logging.config
-import datetime
-import typing
-import json
 import smtplib
-import logging
 import sys
-from discordhandler import DiscordHandler
-from logging.handlers import SMTPHandler
+import typing
 from datetime import datetime as dt
+from logging.handlers import SMTPHandler
 
+import praw
 
-logging.config.fileConfig("basic_logging_config.ini")
-logger = logging.getLogger("eimmk")
+from discordhandler import DiscordHandler
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    "[%(asctime)s][%(name)s][%(levelname)s]: %(message)s", "%Y-%m-%d %H:%M:%S")
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+fh = logging.FileHandler("EiMmK.log", encoding="utf-8")
+fh.setFormatter(formatter)
+fh.setLevel(logging.INFO)
+logger.addHandler(ch)
+logger.addHandler(fh)
+
+config = configparser.ConfigParser()
+try:
+    config.read("config_default.ini", encoding="utf-8")
+    config.read("config.ini", encoding="utf-8")
+except Exception:
+    logger.exception("Unexpected exception while trying to read"
+                     + "config file")
 
 
 # TODO more error logging
@@ -29,27 +46,15 @@ class PostNotFoundException(Exception):
     pass
 
 
-def __load_config():
-    """Loads the configuration from the config.ini file.
-
-    Returns:
-        configparser.SectionProxy: relevant config dictionary
+def __setup_advanced_logging() -> None:
+    """Sets up advanced logging per mail and Discord
     """
-    config = configparser.ConfigParser()
-    config.read("config.ini", encoding="utf-8")
-    return config
-
-
-def __setup_logging() -> logging.Logger:
-    """Sets up logging to console and files.
-    """
-    formatter = logger.handlers[0].formatter
-    if config.getboolean("logging","enable_mail_logging"):
+    if config.getboolean("logging", "enable_mail_logging"):
         mailcfg = dict(config.items("mail_logging"))
         mailhost = (mailcfg["mailhost"], mailcfg["mailport"])
         toaddrs = mailcfg["toaddrs"].split(",")
         credentials = (mailcfg["username"], mailcfg["password"])
-        eh = SMTPHandler(mailhost=mailhost, 
+        eh = SMTPHandler(mailhost=mailhost,
                          fromaddr=mailcfg["fromaddr"],
                          toaddrs=toaddrs,
                          subject=mailcfg["subject"],
@@ -60,14 +65,17 @@ def __setup_logging() -> logging.Logger:
         eh.setFormatter(formatter)
         eh.setLevel(logging.WARNING)
         logger.addHandler(eh)
-    if config.getboolean("logging","enable_discord_logging"):
+
+    if config.getboolean("logging", "enable_discord_logging"):
         dh = DiscordHandler(config["discord_logging"]["username"],
                             config["discord_logging"]["webhook_url"])
         dh.setFormatter(formatter)
         dh.setLevel(logging.WARNING)
         logger.addHandler(dh)
-    logger.debug("Successfully setup logger")
-    return logger
+    logger.debug("Successfully setup advanced loggers")
+    
+# TODO move somewhere else?
+__setup_advanced_logging()
 
 
 def __init_reddit() -> praw.Reddit:
@@ -91,8 +99,9 @@ def get_new_post() -> typing.Optional[praw.models.Submission]:
         typing.Optional[praw.models.Submission]: The new wednesday post
         or None if none available or already fetched today
     """
+
     if __is_date_wednesday(dt.now()):
-        logger.info("It's not wednesday (I think)")
+        logger.debug("It's not wednesday (I think)")
         return None
     try:
         with open("last_posts.json", "r", encoding="utf-8") as f:
@@ -104,7 +113,7 @@ def get_new_post() -> typing.Optional[praw.models.Submission]:
         last_posts = {}
     key = dt.now().strftime("%Y-%m-%d")
     if key in last_posts:
-        logger.info("Already have seen today's post")
+        logger.debug("Already have seen today's post")
         return None
     logger.debug("Fetching latest post")
 
@@ -115,18 +124,24 @@ def get_new_post() -> typing.Optional[praw.models.Submission]:
         return None
 
     logger.info("New post found")
-    last_posts[key] = post
+    last_posts[key] = post.permalink
     with open("last_posts.json", "a", encoding="utf-8") as f:
         json.dump(last_posts, f)
-    return last_posts
+    return post
+
+def get_new_post_url() -> typing.Optional[str]:
+    post = get_new_post()
+    if post is not None:
+        return "https://www.reddit.com" + post.permalink   
 
 
 def __fetch_latest_wednesday_post() -> praw.models.Submission:
+    reddit = __init_reddit()
     latest_submissions = reddit.redditor(
         "SmallLebowsky").submissions.new(limit=20)
     for submission in latest_submissions:
         # BUG Has to check if wednesday in Germany but checks for UTC!
-        if __is_date_wednesday(datetime.utcfromtimestamp(submission
+        if __is_date_wednesday(dt.utcfromtimestamp(submission
                                                          .created_utc)):
             if "Mittwoch" in submission.title:
                 return submission
@@ -171,13 +186,9 @@ def __get_last_wednesday_date(now: dt = None) -> datetime.date:
     now = dt.combine(now.date(), datetime.time())
     return now
 
-config = __load_config()
-__setup_logging()
-reddit = __init_reddit()
-
 if __name__ == "__main__":
-    logger.warning("Test")
-    # post = get_new_post()
-    # if post is None:
-    #     logger.info("No new post found, exiting...")
-    # else:
+    logger.name = "eimmk"
+    try:
+        print(get_new_post_url())
+    except Exception:
+        logger.exception("Unexpected exception caught")
